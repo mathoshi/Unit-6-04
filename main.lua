0f930d9a3b00c85033b291bc6981ad31f5ae3d7c
mathoshi <47573361+mathoshi@users.noreply.github.com>
2019-04-05 13:29:46 -0400

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,9 @@
+-----------------------------------------------------------------------------------------
+--
+-- Created by: Matsuru Hoshi
+-- Created on: Apr 5, 2019
+--
+-- This file will see if you are worthy of getting discount on musuem pricing.
+-----------------------------------------------------------------------------------------
+
+local background = display.setDefault( "background", 244/255, 152/255, 66/255)
